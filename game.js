a9c3cf5b5d659ed3b32e36bfc78a2eed2d49f05a
AKD1242 <101017854+AKD1242@users.noreply.github.com>
2022-03-05 22:15:10 +0300

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   background(1);
-  function touchMoved(){
+  function touchStarted(){
     speed = random(1,5);
     fireworks.push({x:200,y:401,dx:random(-2,2),speed:speed,height:200-(speed*20)});
     return false;
