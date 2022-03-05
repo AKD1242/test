8c973d2750807287357a88b640e5068687300ab0
AKD1242 <101017854+AKD1242@users.noreply.github.com>
2022-03-05 22:29:25 +0300

    Update game.js

--- game.js
+++ game.js
@@ -33,6 +33,5 @@ function draw() {
 function touchStarted(){
   speed = random(1,5);
   fireworks.push({x:200,y:401,dx:random(-2,2),speed:speed,height:200-(speed*20)});
-  let fs = fullscreen();
-  fullscreen(fs);
+  fullscreen(true);
 }
